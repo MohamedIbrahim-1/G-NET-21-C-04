6ea8bf4eb025d96790287f949b413bea5cebf6f6
mohamed ibrahim <mohamed ibrahim@MOHAMEDIBRAHIM>
2026-03-14 08:02:30 +0200

    Question-part#01

---
+++ assignment#04/Enums/DayWeek.cs
@@ -0,0 +1,11 @@
+﻿namespace assignment_04.Enums;
+public enum DayWeek
+{
+    Saturday=0,
+    Sunday=1,
+    Monday=2,
+    Tuesday=3,
+    Wednesday=4,
+    Thursday=5,
+    Friday=6
+}
--- assignment#04/Program.cs
+++ assignment#04/Program.cs
@@ -1,10 +1,78 @@
-﻿namespace assignment_04
+﻿using assignment_04.Enums;
+using ScottPlot.TickGenerators.TimeUnits;
+
+namespace assignment_04
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            #region Question-part#01
+
+            /*
+            Console.Write("Please enter a number (0 - 6): ");
+
+            int.TryParse(Console.ReadLine(), out int number);
+
+            DayWeek day = (DayWeek)number;
+
+            switch (day)
+            {
+                case DayWeek.Saturday:
+                    Console.WriteLine("The day is Saturday");
+                    Console.WriteLine("It's the Weekend");
+                    break;
+
+                case DayWeek.Sunday:
+                    Console.WriteLine("The day is Sunday");
+                    break;
+
+                case DayWeek.Monday:
+                    Console.WriteLine("The day is Monday");
+                    Console.WriteLine("It's a Workday");
+                    break;
+
+                case DayWeek.Tuesday:
+                    Console.WriteLine("The day is Tuesday");
+                    break;
+
+                case DayWeek.Wednesday:
+                    Console.WriteLine("The day is Wednesday");
+                    break;
+
+                case DayWeek.Thursday:
+                    Console.WriteLine("The day is Thursday");
+                    break;
+
+                case DayWeek.Friday:
+                    Console.WriteLine("The day is Friday");
+                    break;
+
+                default:
+                    Console.WriteLine("Invalid number. Please enter a number between 0 and 6.");
+                    break;
+            }
+            
+             */
+
+            #endregion
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         }
     }
 }
